3c354c105fc47d9822b7fbd91d83e6aca69d79a0
BraianStacul <88989886+BraianStacul@users.noreply.github.com>
2024-05-08 21:36:47 -0300

    actualice calculo de suma

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("Hola mundo")
 
-z = 3 # Hacer que este numero lo ingrese el usuario
+z = input("Ingrese un valor: ")
 
 y = 2
 
-z = z+2
-print(f"LLa suma es: {z}")
+# z = z+2
+print(f"La suma es: ",z+y)
