f13cc04bd29ae122c29173c2009f02140f60a76f
BraianStacul <braianstacul000@gmail.com>
2024-05-17 13:51:38 -0300

    creamos tabla clase

--- asistencia_alumnos/apps/clases/models.py
+++ asistencia_alumnos/apps/clases/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Clase(models.Model):
+    fecha = models.DateField()
+
+'''
+class NuevaTabla(models.Model):
+    pass 
+
+    class Meta:
+        db_table = 'nueva_tabla'
+'''
