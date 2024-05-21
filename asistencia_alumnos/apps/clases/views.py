b82359b31562c1853e6609eddcad2f62c54a2e8d
BraianStacul <braianstacul000@gmail.com>
2024-05-21 15:39:35 -0300

    listado de clases

--- asistencia_alumnos/apps/clases/views.py
+++ asistencia_alumnos/apps/clases/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+from .models import Clase
+
 # Create your views here.
 
 def listar_clases(request):
     template_name = 'clases/lista.html'
 
     ctx = {
+        "clases" : Clase.objects.all().order_by("-fecha")
     }
 
     return render(request, template_name, ctx)
