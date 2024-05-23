6252f4cac167ac47347e32e241024e438a4f8d66
BraianStacul <braianstacul000@gmail.com>
2024-05-23 14:16:32 -0300

    mostrar cantidad de presentes en la lista de clases

--- asistencia_alumnos/apps/asistencias/models.py
+++ asistencia_alumnos/apps/asistencias/models.py
@@ -7,4 +7,4 @@ from apps.usuarios.models import Usuario
 
 class Asistencia(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    clase = models.ForeignKey(Clase, on_delete=models.CASCADE)
+    clase = models.ForeignKey(Clase, on_delete=models.CASCADE, related_name="asistencias")
--- asistencia_alumnos/apps/clases/models.py
+++ asistencia_alumnos/apps/clases/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Clase(models.Model):
     fecha = models.DateField()
 
+    def get_cantidad_presentes(self):
+        return self.asistencias.all().count()
+
 '''
 class NuevaTabla(models.Model):
     pass 
